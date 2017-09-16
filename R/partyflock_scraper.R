933d8ad572c5a25dff113a2c829dc64e5ad38b6f
David Jankoski <leledavid@gmail.com>
2017-09-16 03:31:12 +0200

    rewrite of partyflock scraping logic

--- R/partyflock_scraper.R
+++ R/partyflock_scraper.R
@@ -8,94 +8,89 @@ dkrid <- sys::exec_background("sudo", "dockerd")
 # Kill dockerd when fun exits
 on.exit(tools::pskill(dkrid))
 
+# start splash
 library("splashr")
 splashr::start_splash()
-splashr::splash_active()
+# halt if smth goes wrong
+stopifnot(splashr::splash_active())
 
+# partyflock.nl url for parties in ams
 partyflock_url <- "http://partyflock.nl/city/1:Amsterdam"
 
 # render partyflock page
 partyflock_html <-
   splashr::render_html(url = partyflock_url, wait = 10)
 
-# there must be a better way to do this!
-# get to the crappy table
-pf_tab <-
+# extract table into datafame
+partyflock_df <-
   partyflock_html %>%
-  rvest::html_node(css = "#agenda .box-column:nth-child(1)")
+  rvest::html_node("table.partylist.fw.nocellbrd.vtop") %>%
+  rvest::html_table()
 
-# extract text from super crappy table
-pf_text <-
-  pf_tab %>%
-  rvest::html_nodes("tr") %>%
-  rvest::html_text()
+# better names, remove empty
+partyflock_df$X2 <- NULL
 
-# try and get to the links
-# BROKEN - FIX ME !
-rvest::html_nodes(pf_tab, css = "href")
+colnames(partyflock_df) <-
+  c("event_name", "attending", "event_location")
 
-# throw away bunch of empty strings at beginning
-pf_text <- pf_text[stringr::str_detect(pf_text, "")]
+# which rows are actually days
+days_delim <-
+  stringr::str_detect(partyflock_df$event_name, "dag.*[0-9]+.*201[78]")
 
-# indexes of text lines of days
-# e.g. "vandaag, vrijdag 15 september 2017"
-days_delim <- which(stringr::str_detect(pf_text, "^[a-z]"))
+# days idxs in df
+days_delim_idx <- which(days_delim)
+# get only the days out
+dates <- partyflock_df$event_name[days_delim_idx]
 
-# TODO this so messy - untangle spagetti here
-#
-# locate todays delim "vandaag" and throw away the yesterday delim
-# the text at the location of the days_delim's - which one is "vandaag" ?
-today_delim <- which(stringr::str_detect(pf_text[days_delim[1:5]], "vandaag"))
-# the day delimiters at that index is the line in the original text where
-# today starts
-today_delim <- days_delim[today_delim]
+# get idx of today (2 past dates not of interest)
+today_delim_idx <-
+  dates %>%
+  stringr::str_detect("vandaag") %>%
+  which()
+# construct idxs of past days
+past_events <-
+  1:(days_delim_idx[today_delim_idx] - 1)
+# remove them
+partyflock_df <- partyflock_df[-past_events, ]
 
-# lines 1 up to before today_delim line are past info
-past_info_idx <- 1:(today_delim - 1L)
-# remove past info
-pf_text <- pf_text[-past_info_idx]
-
-# update day delimiters to new state
-days_delim <- which(stringr::str_detect(pf_text, "^[a-z]"))
-
-# TODO maybe limit future range to only 14 days ahead ?
-# this should print out date roughly 2 weeks from now
-# maybe minus 1 day (so 15 instead of 14 ?)
-two_weeks_ahead <- pf_text[days_delim[14]]
-
-
-# Switch to dataframe ------------
-
-# convert to dataframe
-pflock <-
-  tibble::data_frame(
-    text = pf_text,
-    day_breaks)
-
-pflock <-
-pflock %>%
+partyflock_df <-
+  partyflock_df %>%
   mutate(
-    day_break = str_detect(text, "vandaag|2017")
-  )
-pflock$day_break[1] <- TRUE
-
-pflock <-
-pflock %>%
-  mutate(
-    day_group = cumsum(day_break)
+    # update wrong rows (got mangled by removing past events)
+    wrong_rows = stringr::str_detect(event_name, "dag.*[0-9]+.*201[78]"),
+    # simple way to attach each date an id
+    date_id = cumsum(wrong_rows)
   )
 
-pflock <-
-pflock %>%
+partyflock_df <-
+  partyflock_df %>%
+  group_by(date_id) %>%
   mutate(
-    event_name = map_chr(str_split(text, pattern = "[0-9]+"), 1)
+    date =
+      stringr::str_extract_all(first(event_name), "[0-9]+.*"),
+    date = unlist(date)
+  ) %>%
+  ungroup() %>%
+  filter(!wrong_rows) %>%
+  mutate(
+    event_location =
+      purrr::map_chr(
+        stringr::str_split(
+          event_location,
+          "AmsterdamNederland"
+        ),
+        1L
+      )
   )
 
-str_split(pf_text[6:10], pattern = "[0-9]") %>%
-  map(1)
+partyflock_df$wrong_rows <- NULL
+partyflock_df$date_id <- NULL
 
-pflock %>% group_by(day_group) %>% filter(!day_break) %>%
-  mutate(event)
+partyflock_df$timestamp <- Sys.time()
 
+# -------
+days_delim2 <-
+  which(stringr::str_detect(partyflock_df$event_name, "dag.*[0-9]+.*201[78]"))
 
-
+partyflock_df$event_name[days_delim2] %>%
+  stringr::str_extract_all("[0-9]+.*")
