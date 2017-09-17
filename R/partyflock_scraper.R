53e5917dca53830cc0a32c711a58cfc0b84a3641
David Jankoski <leledavid@gmail.com>
2017-09-17 23:52:01 +0200

    refactored code a bit, added new cols and store data in db

--- R/partyflock_scraper.R
+++ R/partyflock_scraper.R
@@ -1,16 +1,19 @@
 
 # In order this to work you need to start
 # Rstudio with sudo rstudio !
+library("tidyverse")
 
 # Run dockerd as a background process
 dkrid <- sys::exec_background("sudo", "dockerd")
-
 # Kill dockerd when fun exits
-on.exit(tools::pskill(dkrid))
+#on.exit(tools::pskill(dkrid))
+Sys.sleep(10)
 
-# start splash
+# start splash wait till init done
 library("splashr")
 splashr::start_splash()
+Sys.sleep(10)
+
 # halt if smth goes wrong
 stopifnot(splashr::splash_active())
 
@@ -59,20 +62,48 @@ partyflock_df <-
     # update wrong rows (got mangled by removing past events)
     wrong_rows = stringr::str_detect(event_name, "dag.*[0-9]+.*201[78]"),
     # simple way to attach each date an id
-    date_id = cumsum(wrong_rows)
+    date_id = cumsum(wrong_rows),
+    # convert attending to integer
+    attending = as.integer(attending)
   )
 
 partyflock_df <-
   partyflock_df %>%
+  # for each group (per date id) extract the
+  # actual date out of the event_name
+  # it's the first row of each group (cf. html-table)
   group_by(date_id) %>%
   mutate(
     date =
-      stringr::str_extract_all(first(event_name), "[0-9]+.*"),
-    date = unlist(date)
+      stringr::str_extract(first(event_name), "[0-9]+.*"),
+    # holiday dates have a ", holidayname" at the end
+    # get that out in a sep col
+    date = stringr::str_split(date, ","),
+    holiday = unlist(map(date, 2L)) %||% NA_character_,
+    #holiday = holiday ,
+    date = map_chr(date, 1L),
+    # parse into POSIX
+    date =
+      lubridate::parse_date_time(
+        date,
+        orders = "db!Y",
+        locale = "nl_NL.utf8"
+      ),
+    # get the day of week as labeled factor (e.g. Fri)
+    date_wday = lubridate::wday(
+      date,
+      label = TRUE, abbr = TRUE
+      )
   ) %>%
   ungroup() %>%
+  # dates are extracted, don't need the rows containing
+  # them anymore
   filter(!wrong_rows) %>%
   mutate(
+    # the event location seems to be mangled and
+    # smth like
+    # ParadisoAmsterdamNederlandParadiso
+    # so split-and-pick first part
     event_location =
       purrr::map_chr(
         stringr::str_split(
@@ -80,17 +111,36 @@ partyflock_df <-
           "AmsterdamNederland"
         ),
         1L
-      )
+      ),
+    event_location =
+      ifelse(event_location == "", NA_character_, event_location)
   )
 
+
+# remove unneded cols
 partyflock_df$wrong_rows <- NULL
 partyflock_df$date_id <- NULL
 
+# timestamp of scraping
 partyflock_df$timestamp <- Sys.time()
 
-# -------
-days_delim2 <-
-  which(stringr::str_detect(partyflock_df$event_name, "dag.*[0-9]+.*201[78]"))
+# stop the docker daemon
+tools::pskill(dkrid)
 
-partyflock_df$event_name[days_delim2] %>%
-  stringr::str_extract_all("[0-9]+.*")
+# 6. Dump run into MonetDB --------------------
+
+# store/append data in a db
+stored_ok <- store_data_in_db(partyflock_df, "data/db", "partyflock")
+
+# halt if something went wrong
+stopifnot(stored_ok)
+
+
+
+# -----
+# maybe add some visualisation of the
+# partyflock attendance ?
+
+# ggplot(partyflock_df,
+#        aes(forcats::fct_reorder(factor(event_location), attending), attending))+
+#   geom_col()
